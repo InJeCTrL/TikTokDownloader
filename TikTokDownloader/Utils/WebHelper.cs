c2e90cfc10840be96fd07a7d4f52756e7d8f1c8e
InJeCTrL <dap1933@hotmail.com>
2022-03-07 20:34:52 +0800

    bugfix: tag extract

--- TikTokDownloader/Utils/WebHelper.cs
+++ TikTokDownloader/Utils/WebHelper.cs
@@ -11,21 +11,22 @@ namespace TikTokDownloader.Utils
             try
             {
                 var content = httpClient.GetStringAsync(url).Result;
-                var labelA = "<span class=\"Nu66P_ba\"><span><span><span><span>";
+                var labelA = "<title data-react-helmet=\"true\"> ";
                 var labelB = "<span class=\"CE7XkkTw\">";
                 var labelC = "src%22%3A%22%2F%2F";
+                var labelNickTail = "</span></span></span></span></span></div></a><p class=\"yibO9njB\">";
                 var stopLbl = "[\"src-layouts-index\",\"src-pages-Video-id\"]}</script>";
                 content = content[(content.IndexOf(labelA) + labelA.Length)..content.IndexOf(stopLbl)];
                 var mediaInfo = new Media();
-                mediaInfo.Title = content[..content.IndexOf("</span>")];
+                mediaInfo.Title = content[..content.IndexOf(" - 抖音</title>")];
                 content = content[(content.IndexOf(labelB) + labelB.Length)..];
                 mediaInfo.Like = content[..content.IndexOf("</span>")];
                 content = content[(content.IndexOf(labelB) + labelB.Length)..];
                 mediaInfo.CommentCnt = content[..content.IndexOf("</span>")];
                 content = content[(content.IndexOf(labelB) + labelB.Length)..];
                 mediaInfo.CollectCnt = content[..content.IndexOf("</span>")];
-                content = content[(content.IndexOf(labelA) + labelA.Length)..];
-                mediaInfo.NickName = content[..content.IndexOf("</span>")];
+                var nickSubContent = content[..content.IndexOf(labelNickTail)];
+                mediaInfo.NickName = nickSubContent[(nickSubContent.LastIndexOf("<span>") + 6)..];
                 content = content[(content.IndexOf(labelC) + labelC.Length)..];
                 mediaInfo.MediaURL = content[..(content.IndexOf("vr%3D") + 5)];
                 mediaInfo.MediaURL = $"https://{HttpUtility.UrlDecode(mediaInfo.MediaURL)}";
